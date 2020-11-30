d39f6e8243d865a209b9fa47e4ef443e736ff512
Valdir Cezar <valdircezar312@gmail.com>
2020-11-30 17:05:14 -0300

    Ajustes na criacao da uri para a criacao da Cidade

--- src/main/java/com/ticketlog/resources/CidadeResource.java
+++ src/main/java/com/ticketlog/resources/CidadeResource.java
@@ -23,7 +23,7 @@ public class CidadeResource {
 	@PostMapping
 	public ResponseEntity<Cidade> insert(@RequestBody Cidade obj) {
 		Cidade newObj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(newObj);
 	}
 
