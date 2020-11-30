99333aca1f3a79d9c00ecb03edc3b8fac138bf65
Valdir Cezar <valdircezar312@gmail.com>
2020-11-30 16:37:00 -0300

    Criacao da base de dados automatica usando H@ Database

---
+++ src/main/java/com/ticketlog/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.ticketlog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ticketlog.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
---
+++ src/main/java/com/ticketlog/services/DBService.java
@@ -0,0 +1,26 @@
+package com.ticketlog.services;
+
+import java.text.ParseException;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ticketlog.domain.Estado;
+import com.ticketlog.repositories.EstadoRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	public void instantiateTestDatabase() throws ParseException {
+		Estado estado1 = new Estado(null, "Rio Grande do Sil");
+		Estado estado2 = new Estado(null, "Santa Catarina");
+		Estado estado3 = new Estado(null, "Panará");
+
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2, estado3));
+	}
+
+}
