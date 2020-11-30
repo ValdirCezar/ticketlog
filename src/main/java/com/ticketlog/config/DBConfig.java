c89a504a7ecb00b0e07dace0ea565bfcbfc5c282
Valdir Cezar <valdircezar312@gmail.com>
2020-11-30 16:22:22 -0300

    Classes de dominio criadas e relacionadas

---
+++ src/main/java/com/ticketlog/config/DBConfig.java
@@ -0,0 +1,15 @@
+package com.ticketlog.config;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;;
+
+@Configuration
+public class DBConfig implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
+}
---
+++ src/main/java/com/ticketlog/domain/Cidade.java
@@ -0,0 +1,101 @@
+package com.ticketlog.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cidade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private Long populacao;
+	private Double custoCidadeUS; 
+	
+	@ManyToOne
+	@JoinColumn(name = "estado_id")
+	private Estado estado;
+
+	public Cidade() {
+		super();
+	}
+
+	public Cidade(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getPopulacao() {
+		return populacao;
+	}
+
+	public void setPopulacao(Long populacao) {
+		this.populacao = populacao;
+	}
+
+	public Double getCustoCidadeUS() {
+		return custoCidadeUS;
+	}
+
+	public void setCustoCidadeUS(Double custoCidadeUS) {
+		this.custoCidadeUS = custoCidadeUS;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+}
