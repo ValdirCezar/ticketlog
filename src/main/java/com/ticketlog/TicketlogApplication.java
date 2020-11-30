38fe4de75d7262ef2da55bf322a37de82e61d7ab
Valdir Cezar <valdircezar312@gmail.com>
2020-11-30 15:48:37 -0300

    initial commit

---
+++ src/main/java/com/ticketlog/TicketlogApplication.java
@@ -0,0 +1,13 @@
+package com.ticketlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketlogApplication.class, args);
+	}
+
+}
