22b6dfdb7ffd43d18ac25012787aa3bdb82d1690
Valdir Cezar <valdircezar312@gmail.com>
2020-12-01 08:40:03 -0300

    ajustes de Cors

--- src/main/java/com/ticketlog/resources/CidadeResource.java
+++ src/main/java/com/ticketlog/resources/CidadeResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.ticketlog.services.CidadeService;
 
 @RestController
 @RequestMapping("/api/cidades")
+@CrossOrigin("*")
 public class CidadeResource {
 	
 	// !!!!!!! Implementar os tratamentos de exceção após finalizar a api
@@ -26,7 +28,7 @@ public class CidadeResource {
 	@Autowired
 	private CidadeService service;
 	
-	@PostMapping()
+	@PostMapping("/estado={id}")
 	public ResponseEntity<Cidade> insert(@RequestBody Cidade obj) {
 		Cidade newObj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
